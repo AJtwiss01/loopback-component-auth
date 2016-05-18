b70dbedee08f964faeab57a419a6535e56a14858
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2016-05-18 12:08:36 +0200

    reduce AuthProvider model to actually exposed information
    * removed information from AuthProvider Model that is not actually exposed
    * implement remote methods to list `login` and `link` providers

--- models/authProvider.js
+++ models/authProvider.js
@@ -1,7 +1,29 @@
 'use strict';
 
-const logger = require('../lib/logger');
+// const logger = require('../lib/logger');
 const providerRegistry = require('../lib/provider-registry');
+
+function providerConfigToModel(config) {
+  const model = {
+    disabled: !!config.provider.disabled,
+    name: config.name,
+    authPath: config.route.auth.path,
+    authMethod: config.route.auth.method,
+    link: !!config.provider.link,
+    responseType: config.provider.json ? 'json' : 'redirect',
+  };
+
+  if (!/(HEAD|GET)/i.test(model.authMethod)) {
+    model.authBodyFormat = config.route.auth.bodyParser;
+  }
+
+  if (model.responseType === 'redirect') {
+    model.successRedirectUrl = config.strategy.successRedirect;
+    model.failureRedirectUrl = config.strategy.failureRedirect;
+  }
+
+  return model;
+}
 
 function modelCustomizer(Model) {
   Model.once('dataSourceAttached', () => {
@@ -9,24 +31,70 @@ function modelCustomizer(Model) {
     // const connectorSettings = connector.settings;
 
     /* eslint-disable no-param-reassign */
-    Model.find = function find(filter, callback) {
-      logger.warn('`AuthProvider.find()` method has no filter methods implemented');
+    Model.findLoginProviders = function findLoginProviders(includeDisabled, callback) {
       // @TODO: read user's roles and decide weather to show clientSecret and clientID
       // or even make it more generic: read list of "protected" properties from provider config
       // and remove those from response list
-      return callback(null, providerRegistry.list());
+
+      const result = providerRegistry
+        .list()
+        // remove all providers where `link` is set to `true`
+        .filter((config) => {
+          return !config.provider.link;
+        })
+        // remove `disabled` providers if not requested otherwise
+        .filter((config) => {
+          if (includeDisabled) {
+            return true;
+          }
+          return !config.provider.disabled;
+        })
+        .map((config) => {
+          return providerConfigToModel(config);
+        });
+
+      return callback(null, result);
     };
 
+    Model.findLinkProviders = function findLinkProviders(includeDisabled, callback) {
+      // @TODO: read user's roles and decide weather to show clientSecret and clientID
+      // or even make it more generic: read list of "protected" properties from provider config
+      // and remove those from response list
+
+      const result = providerRegistry
+        .list()
+        // remove all providers where `link` is set to `true`
+        .filter((config) => {
+          return config.provider.link;
+        })
+        // remove `disabled` providers if not requested otherwise
+        .filter((config) => {
+          if (includeDisabled) {
+            return true;
+          }
+          return !config.provider.disabled;
+        })
+        .map((config) => {
+          return providerConfigToModel(config);
+        });
+
+      return callback(null, result);
+    };
+    /* eslint-enable no-param-reassign */
+
     Model.remoteMethod(
-      'find', {
-        description: 'List authentication providers',
+      'findLoginProviders', {
+        description: 'List authentication providers that support login requests',
         notes: '',
         isStatic: true,
         accessType: 'READ',
         accepts: [{
-          arg: 'filter',
-          type: 'object',
-          description: 'Filter defining fields, where, include, order, offset, and limit',
+          arg: 'includeDisabled',
+          type: 'Boolean',
+          default: false,
+          http: {
+            source: 'query',
+          },
         }],
         returns: {
           root: true,
@@ -34,12 +102,37 @@ function modelCustomizer(Model) {
         },
         http: {
           verb: 'get',
-          path: '/',
+          path: '/login',
           status: 200,
           errorStatus: 500,
         },
       });
-    /* eslint-enable no-param-reassign */
+
+    Model.remoteMethod(
+      'findLinkProviders', {
+        description: 'List authentication providers that support linking auser accounts',
+        notes: '',
+        isStatic: true,
+        accessType: 'READ',
+        accepts: [{
+          arg: 'includeDisabled',
+          type: 'Boolean',
+          default: false,
+          http: {
+            source: 'query',
+          },
+        }],
+        returns: {
+          root: true,
+          type: ['AuthProvider'],
+        },
+        http: {
+          verb: 'get',
+          path: '/link',
+          status: 200,
+          errorStatus: 500,
+        },
+      });
   });
 }
 
